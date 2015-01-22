0cd3b12d94691457dcb04ba48af3933f76bdbe51
daghan_acay <lacay@unimelb.edu.au>
2015-01-22 17:39:49 +1100

    multithreading with streams and lambda

--- java8Project/src/test/java/examples/lambda/ArrayLambdasTest.java
+++ java8Project/src/test/java/examples/lambda/ArrayLambdasTest.java
@@ -1,5 +1,8 @@
 package examples.lambda;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -15,6 +18,17 @@ public class ArrayLambdasTest {
   @Test
   public void test() {
     classUnderTest.printArray();
+  }
+
+  @Test
+  public void interestingCase_coordianateRunnablesWithStreams() {
+    // we create a list of two runnables in a list and use stream to create threads
+    final List<Runnable> runnables = Arrays.asList(() -> System.out.println("running first thread"),
+        () -> System.out.println("running second thread"));
+    System.out.println("staring ordered run");
+    runnables.parallelStream().forEachOrdered(r -> r.run());
+    System.out.println("staring random run");
+    runnables.parallelStream().forEach(r -> r.run());
   }
 
 }
